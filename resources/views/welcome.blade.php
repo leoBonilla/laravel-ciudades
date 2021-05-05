0d739cb8cc5b00421510845920fa67beb2cccbb3
leonardo Bonilla <leonardo.bonilla@zenbyte.cl>
2021-05-05 06:23:23 -0400

    remove front from laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Regiones y comunas</title>
-        <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
+        <title>Api</title>
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Localizador</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-
-    </ul>
-  </div>
-</nav>
-        <div id="example"></div>
-        <script src="{{mix('js/app.js')}}" ></script>
+        Silence is Golden!!
     </body>
 </html>
 
